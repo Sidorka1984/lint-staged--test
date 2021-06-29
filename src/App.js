9f6242c7e1e7c1d5cfae81e6151d88ea7267af67
Olga Ulyanchyk <sidorka.olka@gmail.com>
2021-06-29 15:08:45 +0300

    новый материал

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from "./logo.svg";
-import "./App.css";
+// import logo from "./logo.svg";
+import React, { Component } from 'react';
+import './App.css';
+import Counter from './components/Counter';
+import Dropdown from './components/Dropdown';
+import ColorPicker from './components/ColorPicker';
+import TodoList from './components/TodoList';
+import initialTodos from './todos.json';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const colorPickerOptions = [
+  { label: 'red', color: '#F44336' },
+  { label: 'green', color: '#4CAF50' },
+  { label: 'blue', color: '#2196F3' },
+  { label: 'grey', color: '#607D8B' },
+  { label: 'pink', color: '#E91E63' },
+  { label: 'indigo', color: '#3F5185' },
+];
+
+class App extends Component {
+  state = {
+    todos: initialTodos,
+  };
+  deleteTodo = todoId => {
+    this.setState(prevState => ({
+      todos: prevState.todos.filter(todo => todo.id !== todoId),
+    }));
+  };
+
+  render() {
+    const { todos } = this.state;
+
+    const totalTodoCount = todos.length;
+
+    const completedTodoCount = todos.reduce(
+      (total, todo) => (todo.completed ? total + 1 : total),
+      0,
+    );
+
+    return (
+      <>
+        <h1>Состояние документа</h1>
+        <div>
+          <p>Общее кол-во: {totalTodoCount}</p>
+          <p>Кол-во выполненных: {completedTodoCount}</p>
+        </div>
+        <TodoList todos={todos} onDeleteTodo={this.deleteTodo} />
+        <ColorPicker options={colorPickerOptions} />
+        <Counter />
+        <Dropdown />
+      </>
+    );
+  }
 }
+// const App = () => (
+//   <>
+//     <h1>Состояние документа</h1>
+//     <ColorPicker options = {colorPickerOptions} />
+//     <Counter />
+//     <Dropdown />
+//     <TodoList />
+
+//   </>
+// )
 
 export default App;
